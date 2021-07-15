5464e39c4d06769f552d181ab82175c768a7fd10
Hoybaby <mbartek436@gmail.com>
2021-07-15 15:12:28 -0400

    setting up my impors from my config files that i created/grabbed from my resource I am using to help me learn react better. Also just applying what I know or trying to know later'

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+// configuration
+
+import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config'
+
+// Hook
+
+
+// Image
+
+import NoImage from '../images/no_image.jpg';
+
+const Home = () => {
+    return <div>
+        Home Page
+    </div>
+}
