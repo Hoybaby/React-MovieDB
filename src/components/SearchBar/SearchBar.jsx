b3539a2a22532960a0ebe5db9baf96484af006c1
Hoybaby <mbartek436@gmail.com>
2021-07-16 18:40:28 -0400

    after adding some of the code like I have been doing with my new habit, The searchbar is almost done

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,29 @@
 import React, {useState, useEffect, useRef} from 'react';
 
+// styles
 import { Wrapper, Content } from './SearchBar.styles'
+
+// image
+
+import searchIcon from '../../images/search-icon.svg'
 
 // the useState will create a controlled component which have the input field.
 // useEffect will trigger when local state changes and update search term
 // useRef will help specify the useEffect and make somethings we dont want to happen
+
+// destructing a prop again
+const SearchBar = ({setSearchTerm}) => {
+    return(
+        <Wrapper>
+            <Content>
+                <img src={searchIcon} alt='search-icon'></img>
+                <input type="text" 
+                placeholder='Search Movie'
+                />
+            </Content>
+        </Wrapper>
+
+    )
+}
+
+export default SearchBar;
