51b8518dd3472762dc526b1454fabb3d3a22c5fc
Hoybaby <mbartek436@gmail.com>
2021-07-18 13:03:01 -0400

    working on the styling. finally after toying it and making things, got it to work

--- src/components/SearchBar/SearchBar.styles.js
+++ src/components/SearchBar/SearchBar.styles.js
@@ -1,6 +1,23 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    display:flex;
+    align-items: center;
+    height: 100px;
+    background: var(--darkGrey);
+    padding: 0 20px;
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+
+    position: relative;
+    max-width: var(--maxWidth);
+    width: 100%;
+    height: 55px;
+    background: var(--medGrey);
+    margin: 0 auto;
+    border-radius: 40px;
+    color: var(--white);
+
+`;
 
