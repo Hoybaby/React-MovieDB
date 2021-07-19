82cd77913e633bb6746caa62bab3a92ee66956ee
Hoybaby <mbartek436@gmail.com>
2021-07-19 12:19:58 -0400

    just adding the browserRouter and others in the App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 // import './App.css';
+
+// Routing
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+// styles
 import {GlobalStyle} from './GlobalStyles'
+
+// components
 import Header from './components/Header/Header';
 import Home from './components/Home';
 
