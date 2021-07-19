aa33ed41938ad4e19356d0fcd711eeaaf027f1de
Hoybaby <mbartek436@gmail.com>
2021-07-19 13:37:31 -0400

    after getting the basic routing done. Now I am just trying to set up the page what it will look like when it goes to the movie of that specifc click

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
-const Movie = () => 
-    <div>
-        Movie
-    </div>
+// config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+
+// componenets
+import Grid from './Grid/Grid';
+import { Spinner } from './Spinner/Spinner';
+
+// Hook
+
+
+// Image
+import NoImage from '../images/no_image.jpg'
+
+const Movie = () => {
+
+    return(
+        <div>
+            <div>Movie Page</div>
+        </div>
+    )
+}
+    
 
 
 export default Movie;
