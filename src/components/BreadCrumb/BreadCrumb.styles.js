4242b53ef591c4b10d3710a262ebcb0ed9352914
Hoybaby <mbartek436@gmail.com>
2021-07-19 15:32:53 -0400

    added some styling to the breadcrumb

--- src/components/BreadCrumb/BreadCrumb.styles.js
+++ src/components/BreadCrumb/BreadCrumb.styles.js
@@ -1,6 +1,16 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 70px;
+    background: var(--medGrey);
+    color: var(--white);
+    
+
+`;
 
 export const Content = styled.div``;
 
