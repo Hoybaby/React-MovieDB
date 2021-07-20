21e1912dad97eff6ef2347f5291b0cfd48a18952
Hoybaby <mbartek436@gmail.com>
2021-07-19 22:49:17 -0400

    adding a big component which will handle what will be shown on the move.js page. this will show the actprs, directos abd the movie image when a user glicks onto the thumbnail

---
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Wrapper, Content, Text } from './MovieInfo.styles';
+
+import Thumb from '../Thumb/Thumb';
+
+// config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config';
+
+// fallback image
+import NoImage from '../../images/no_image.jpg';
