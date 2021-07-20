ce0a6b48e55067de1fab05cfd2dba8f6de97604a
Hoybaby <mbartek436@gmail.com>
2021-07-19 22:57:24 -0400

    did the rest of the standard imports and exports i need. starting building out the jsx

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -9,3 +9,18 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config';
 
 // fallback image
 import NoImage from '../../images/no_image.jpg';
+
+const MovieInfo = ({movie}) => (
+    // passing in a prop
+    <Wrapper backdrop={movie.backdrop_path}>
+        <Content>
+            <Text>
+
+            </Text>
+        </Content>
+    </Wrapper>
+)
+
+
+
+export default MovieInfo;
